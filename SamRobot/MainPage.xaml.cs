7ae44b4b3b9d43a090eb253a90b78d0810e76225
tiago <tiago@TIAGOEDGE>
2016-12-30 10:26:56 -0300

    Fix statusbar and update Assets

--- SamRobot/MainPage.xaml.cs
+++ SamRobot/MainPage.xaml.cs
@@ -16,16 +16,26 @@ using Windows.Web.Http;
 using Windows.Data.Json;
 using Windows.System.Profile;
 using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.UI.ViewManagement;
+using Windows.UI;
 
 namespace SamRobot
-{
-	// By Tiago Danin
+{ 
+	// App By Tiago Danin
 	// Chatbot API by Program-o.com
 	public sealed partial class MainPage : Page
 	{
 		public MainPage()
 		{
 			this.InitializeComponent();
+			if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+			{
+				var statusBar = StatusBar.GetForCurrentView();
+				var colorBar = Application.Current.Resources["SystemControlBackgroundChromeMediumBrush"] as SolidColorBrush;
+
+				statusBar.BackgroundColor = colorBar.Color;
+				statusBar.BackgroundOpacity = 1;
+			}
 		}
 
 		private async void programO(string Input, bool key)
@@ -44,7 +54,7 @@ namespace SamRobot
 				txtOutputTwo.Text = Input;
 			}
 
-			string param = "?bot_id=6&convo_id=sam" + Id.Substring(0, 10) + "&format=json&say=" + Input;
+			string param = "?bot_id=6&convo_id=sam" + Id.Substring(0, 12) + "&format=json&say=" + Input;
 
 			// Send Request
 			Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
